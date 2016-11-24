0d518b10a0d435f88cc218cd9007f8ce744d24b0
FelipeGerbelli <tkzecow@gmail.com>
2016-11-24 14:22:20 -0200

    Revert "Arquivos"
    This reverts commit 637d553ef7d0cbc49162e4a603cb733a6af4a4a3.

--- constants/sql_constants.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-if (!defined('HOSTNAME_SQL')) define('HOSTNAME_SQL', 'localhost');
-if (!defined('USERNAME')) define('USERNAME', 'root');
-if (!defined('PASSWORD')) define('PASSWORD', '');
-if (!defined('DATABASE_NAME')) define('DATABASE_NAME', 'homologacao');
-if (!defined('QUERY_SELECT_ALL')) define('QUERY_SELECT_ALL', 'SELECT * FROM homologacao');
-
--- phpUtils/functions.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Felipe Gerbelli
- * Date: 21/11/2016
- * Time: 14:04
- */
